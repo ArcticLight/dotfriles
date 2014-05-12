1f68760a04bd1cd5ac8dd0758fd8cc1d70d34f6f
Max <mattrulz127@gmail.com>
2014-05-12 00:40:59 -0400

    Update dotfriles-with-sudo.sh

--- dotfriles-with-sudo.sh
+++ dotfriles-with-sudo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Updating APT indexes..."
-apt-get update > ~/.dotfriles/apt-update-log.log
+apt-get update 1>&2
 echo "Running apt-get upgrade (to bring you up to date)..."
 echo "(THIS COULD TAKE A LONG TIME, PLEASE BE PATIENT)"
-apt-get -y upgrade > ~/.dotfriles/apt-upgrade-log.log
+apt-get -y upgrade 1>&2
 echo "Now installing git, zsh, and vim..."
-apt-get -y install git zsh vim > ~/.dotfriles/apt-install-log.log
+apt-get -y install git zsh vim 1>&2
 echo "Done!"
